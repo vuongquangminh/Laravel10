c82a6b5bc800138b8bdcf7ad30be9d5cf3e8e5db
vuongquangminh <vuongquangminh120802@gmail.com>
2024-01-10 17:13:38 +0700

    phương thức booted() chạy khi gọi đến model. Ở đây nó thưucj hiện lấy ra tất cả các task của người đăng nhập chứ không lấy tất cả

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Task extends Model
 {
@@ -19,5 +21,11 @@ class Task extends Model
     public function creator(): BelongsTo
     {
         return $this->belongsTo(User::class, 'creator_id');
+    }
+    protected static function booted(): void
+    {
+        static::addGlobalScope('creator_id', function (Builder $builder) {
+            $builder->where('creator_id', Auth::id());
+        });
     }
 }
